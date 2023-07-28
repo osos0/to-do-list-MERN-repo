2f8f670b06d3bdbf4d3b12fe9042138d7ed2830e
unknown <osama_magdy0@yahoo.com>
2023-07-28 09:57:18 +0300

    backend fixing

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://signin-mern.netlify.app/"],
+    origin: ["http://localhost:3000", "https://signin-mern.netlify.app"],
   })
 );
 
